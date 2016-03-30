26457981a417328ecdaa3cf0de0010bc96f6348f
dddailing <qzyz_dailing@163.com>
2016-03-30 21:01:38 +0800

    change port

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"git.oschina.net/dddailing/schoolapp/controllers"
 	_ "git.oschina.net/dddailing/schoolapp/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.RESTRouter("/login", &controllers.LoginController{})
 	beego.Run()
 }
-
