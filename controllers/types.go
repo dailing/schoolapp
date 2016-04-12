2c9ee42c8a35d454659c4156f004a86c47e9d2d9
dddailing <qzyz_dailing@163.com>
2016-04-12 18:31:15 +0800

    Add html files to this project. adjusting code to get more reasonable structure.

--- controllers/types.go
+++ controllers/types.go
@@ -5,13 +5,19 @@ type TypeMataData struct {
 	Device    string
 }
 
+type TypeRegularResp struct {
+	MataData TypeMataData `json:"mataData"`
+	Status   TypeStatus   `json:"status"`
+}
+
 type TypeUserInfo struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
-	NickName string `json:nickname`
+	NickName string `json:"nickname"`
 	ID       string `json:"ID"`
-	Phone    string `json:"Phone"`
-	Email    string `json:"Email"`
+	Phone    string `json:"phone"`
+	Email    string `json:"email"`
+	Coins    string `josn:"coins"`
 }
 
 type TypeStatus struct {
