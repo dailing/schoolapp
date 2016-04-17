72ac0d157152a1c7ba726b5c45f5b83419ba8a75
dddailing <qzyz_dailing@163.com>
2016-04-17 16:17:47 +0800

    adding uncomit files from last commit

---
+++ controllers/imgGet.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"io/ioutil"
+
+	"encoding/json"
+	"github.com/astaxie/beego"
+)
+
+type ImgGetController struct {
+	beego.Controller
+}
+
+func (c *ImgGetController) Post() {
+	beego.Trace("img get api")
+	imgInfo := TypeImgResp{}
+	body := c.Ctx.Input.CopyBody(1024 * 1024)
+	err := json.Unmarshal(body, &imgInfo)
+	ErrReport(err)
+	if err != nil {
+		c.Abort("400")
+		return
+	}
+	img, err := ioutil.ReadFile(imgPath + imgInfo.ImageID)
+	ErrReport(err)
+	if err != nil {
+		c.Abort("500")
+		return
+	}
+	//c.Ctx.ResponseWriter.Write(img)
+	err = c.Ctx.Output.Body(img)
+	ErrReport(err)
+
+}
